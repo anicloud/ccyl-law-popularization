1f4695c51cf1e5373caab5ddaf91bbd7020c185f
localSummer <roamingcode@163.com>
2017-12-05 11:01:14 +0800

    update

--- web/src/main/webapp/src/routes/index.js
+++ web/src/main/webapp/src/routes/index.js
@@ -19,7 +19,6 @@ const Routes = () => (
         <Route path='/404' component={AsyncPageNotFound} />
 
         <Redirect exact from='/home/index' to='/' />
-        <Redirect exact from='/home/404' to='/' />
         <Redirect from='*' to='/404' />
     </Switch>
 );
