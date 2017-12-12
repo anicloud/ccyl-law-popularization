debe0ce04dfbe59fab391833f0d0c7c63af9ebca
LiHui <812486664@qq.com>
2017-12-12 18:05:28 +0800

    微信用户登陆

--- service/src/main/java/com/ani/ccyl/leg/service/infrastructure/shiro/CustomCredentialsMatcher.java
+++ service/src/main/java/com/ani/ccyl/leg/service/infrastructure/shiro/CustomCredentialsMatcher.java
@@ -1,6 +1,8 @@
 package com.ani.ccyl.leg.service.infrastructure.shiro;
 
+import com.ani.ccyl.leg.commons.dto.AccountDto;
 import com.ani.ccyl.leg.commons.utils.Encrypt;
+import com.ani.ccyl.leg.service.service.facade.AccountService;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.UsernamePasswordToken;
@@ -10,13 +12,23 @@ import org.apache.shiro.authc.credential.SimpleCredentialsMatcher;
  * Created by lihui on 17-6-27.
  */
 public class CustomCredentialsMatcher extends SimpleCredentialsMatcher {
+	private AccountService accountService;
+
+	public AccountService getAccountService() {
+		return accountService;
+	}
+
+	public void setAccountService(AccountService accountService) {
+		this.accountService = accountService;
+	}
+
 	public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
 		if(info == null) {
 			return false;
 		}
-		UsernamePasswordToken usertoken = (UsernamePasswordToken) token; 
-		
-		Object tokenCredentials = Encrypt.md5hash(String.valueOf(usertoken.getPassword()),usertoken.getUsername());
+		UsernamePasswordToken usertoken = (UsernamePasswordToken) token;
+		AccountDto accountDto = accountService.findByAccountName(usertoken.getUsername());
+		Object tokenCredentials = Encrypt.md5hash(String.valueOf(usertoken.getPassword()),accountDto.getOpenId());
 		Object tokenCredentials1 = String.valueOf(usertoken.getPassword());
 		Object accountCredentials = getCredentials(info);
 
