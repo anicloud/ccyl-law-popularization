d170823de272966ac8f8f0089458e90c2c7d02e3
localSummer <roamingcode@163.com>
2017-12-13 14:43:14 +0800

    update

--- web/src/main/webapp/src/routes/index.js
+++ web/src/main/webapp/src/routes/index.js
@@ -7,15 +7,19 @@ import AsyncCounter from '../views/AsyncCounter';
 import AsyncZen from '../views/AsyncZen';
 import AsyncElapse from '../views/AsyncElapse';
 import AsyncRoute from '../views/AsyncRoute';
-
+/*注册信息*/
 import Regist from '../views/Regist';
+/*每日答题*/
 import AnswerQuestion from '../views/AnswerQuestion';
+/*赛事说明*/
+import Description from '../views/Description';
 
 const Routes = () => (
     <Switch>
         <Route exact path='/' component={App} />
         <Route exact path='/regist' component={Regist} />
         <Route path='/answer' component={AnswerQuestion} />
+        <Route path='/description' component={Description} />
 
         <Route exact path='/home' component={Home} />
         <Route path='/counter' component={AsyncCounter} />
