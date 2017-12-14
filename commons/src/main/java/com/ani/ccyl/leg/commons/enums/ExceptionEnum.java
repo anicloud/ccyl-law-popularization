fa3c7da6ac97dcb0b9596a7bafafae787d6df1a9
LiHui <812486664@qq.com>
2017-12-14 13:52:35 +0800

    获取jssdk签名

--- commons/src/main/java/com/ani/ccyl/leg/commons/enums/ExceptionEnum.java
+++ commons/src/main/java/com/ani/ccyl/leg/commons/enums/ExceptionEnum.java
@@ -22,7 +22,7 @@ public enum ExceptionEnum {
         this.value = value;
     }
 
-    ExceptionEnum(String value) {
+    private ExceptionEnum(String value) {
         this.value = value;
     }
 }
