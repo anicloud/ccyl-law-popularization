7ca12d5e417d6f4f67d5dceab4961bd55b467dcd
localSummer <roamingcode@163.com>
2017-12-14 15:35:49 +0800

    update

---
+++ web/src/main/webapp/src/views/Options.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Options extends Component {
+    constructor(props) {
+        super(props);
+        console.log(this.props);
+    }
+    render() {
+        return (
+            <div>Options</div>
+        )
+    }
+}
+
+export default Options;
