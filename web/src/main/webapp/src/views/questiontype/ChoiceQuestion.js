729658953389e6379f6e28055b80d2ee6b8df595
localSummer <roamingcode@163.com>
2017-12-18 17:16:55 +0800

    update

--- web/src/main/webapp/src/views/questiontype/ChoiceQuestion.js
+++ web/src/main/webapp/src/views/questiontype/ChoiceQuestion.js
@@ -5,46 +5,110 @@
  * 选择题组件
  */
 import React,{Component} from "react";
-import {Form,FormCell,CellHeader,CellBody,Radio,Button} from 'react-weui';
+import {Form,FormCell,CellHeader,CellBody,Radio,Button, Toast} from 'react-weui';
 import "../../media/styles/question.less"
+import {connect} from 'react-redux';
+import axios from 'axios';
 
 class ChoiceQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            question: null,
+            answer: null,
+            value: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    componentDidMount() {
+        console.log(this.props.question);
+        this.setState({
+            question: this.props.question
+        });
+    }
+    handleChange(e) {
+        let value = e.target.value;
+        this.setState({
+            value
+        });
+    }
+    handleSubmit() {
+        let _this = this;
+        const {id} = _this.state.question;
+        const {host, handleShowNext} = _this.props;
+        axios.get(`${host}/question/verify?id=${id}&answer=${_this.state.value}`).then(function (response) {
+            if (response.data.state === 0) {
+                _this.setState({
+                    answer: response.data.data
+                }, function () {
+                    handleShowNext();
+                });
+            }
+        }).catch(function (errors) {
+            console.log(errors);
+        })
+    }
     render() {
+        let question = this.state.question;
+        let result = this.state.answer;
         return (
             <div className="question">
-                <h3 className="text-center question title">
-                    <span>第一题</span>
-                </h3>
-                <p>
-                    我国的刑罚分为主刑和附加刑，其中附加刑包括罚金、拘役和没收财产
-                    dknoisdhngoinsdfibso
-                    sdgosidhgoais
-                    sdgfosidbgoasibdg;oasidb
-                </p>
-                <Form checkbox>
-                    <FormCell checkbox>
-                        <CellHeader>
-                            <Radio name="radio1" value="A"/>
-                        </CellHeader>
-                        <CellBody>Option 1</CellBody>
-                    </FormCell>
-                    <FormCell checkbox>
-                        <CellHeader>
-                            <Radio name="radio1" value="B" defaultChecked/>
-                        </CellHeader>
-                        <CellBody>Option 2</CellBody>
-                    </FormCell>
-                    <FormCell checkbox>
-                        <CellHeader>
-                            <Radio name="radio1" value="C" defaultChecked/>
-                        </CellHeader>
-                        <CellBody>Option 3</CellBody>
-                    </FormCell>
-                </Form>
+                {
+                    question? (
+                        <div>
+                            <h3 className="text-center question title">
+                                <span>第{question.order}题</span>
+                            </h3>
+                            <p className='content'>
+                                {question.content}
+                            </p>
+                            <Form checkbox className='form'>
+                                <FormCell checkbox>
+                                    <CellHeader>
+                                        <Radio name="radio1" value="A" onChange={(e) => {this.handleChange(e)}}/>
+                                    </CellHeader>
+                                    <CellBody>{question.optionOne}</CellBody>
+                                </FormCell>
+                                <FormCell checkbox>
+                                    <CellHeader>
+                                        <Radio name="radio1" value="B" onChange={(e) => {this.handleChange(e)}} />
+                                    </CellHeader>
+                                    <CellBody>{question.optionTwo}</CellBody>
+                                </FormCell>
+                                <FormCell checkbox>
+                                    <CellHeader>
+                                        <Radio name="radio1" value="C" onChange={(e) => {this.handleChange(e)}}/>
+                                    </CellHeader>
+                                    <CellBody>{question.optionThree}</CellBody>
+                                </FormCell>
+                            </Form>
+                            {
+                                result? (result.isCorrect? (
+                                    <p className='text-danger result'>恭喜你答对了，积分+5</p>
+                                ) : (
+                                    <p className='text-danger result'>很遗憾，正确答案为： {result.answer}</p>
+                                )) : (null)
+                            }
+                            {
+                                result? (null) : (
+                                    <Button className='question-button' type="primary" plain onClick={this.handleSubmit}>提交</Button>
+                                )
+                            }
+                        </div>
+                    ) : (
+                        <Toast icon="loading" show={true}>Loading...</Toast>
+                    )
+                }
 
-                <Button className='questionButton' type="primary" plain>下一题</Button>
             </div>
         )
     }
 }
-export default ChoiceQuestion;
+
+function mapStateToProps(state) {
+    return {
+        host: state.host
+    };
+}
+
+export default connect(mapStateToProps)(ChoiceQuestion);
