52bee83e99dcfec32496996bd7c2bc49e38fbed2
sunminggui <757596945@qq.com>
2017-12-19 13:36:32 +0800

    update

--- web/src/main/webapp/src/views/MyScore.js
+++ web/src/main/webapp/src/views/MyScore.js
@@ -4,6 +4,7 @@
 /*我的积分页面*/
 import React,{Component} from "react";
 import "../media/styles/myscore.less";
+import {Button} from 'react-weui';
 
 class MyScore extends Component{
     constructor(props){
@@ -12,16 +13,42 @@ class MyScore extends Component{
 
     render() {
         return(
-          <div className="MyScore score-bg">
-              <div className="cloud">
-                <h2 className="text-center h2 title">
-                    <span>我的积分</span>
-                </h2>
-                <h3 className="text-center h3 title">
-                    <span>第一天第三场</span>
-                </h3>
+            <div>
+              <div className="MyScore score-bg">
+                  <div className="cloud">
+                      <h2 className="text-center h2 title">
+                          <span>我的积分</span>
+                      </h2>
+                      <h3 className="text-center h3 title">
+                          <span>111</span>
+                      </h3>
+                  </div>
               </div>
-          </div>
+              <div className="bottomDiv">
+                      <div className="buttonDiv">
+                          <Button>123</Button>
+                          <Button>456</Button>
+                      </div>
+                      <div className="scoreTask">
+                         <span>积分任务</span>
+                      </div>
+                      <div className="task">
+                          <span>签到</span>
+                          <span className="desc">+5积分/次</span>
+                          <Button>签到</Button>
+                      </div>
+                      <div className="task">
+                          <span>普法小奖状</span>
+                          <span className="desc">+5积分/次</span>
+                          <Button>查看</Button>
+                      </div>
+                      <div className="task">
+                          <span>加分包</span>
+                          <span className="desc">+5能量/次</span>
+                          <Button>查看</Button>
+                      </div>
+                </div>
+            </div>
         );
     }
 }
