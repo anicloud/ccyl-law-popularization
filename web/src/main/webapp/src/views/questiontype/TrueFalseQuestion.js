d5aee5bd44841b3e0bcdb01f694d739fcc3ae81d
localSummer <roamingcode@163.com>
2017-12-21 10:46:57 +0800

    update

--- web/src/main/webapp/src/views/questiontype/TrueFalseQuestion.js
+++ web/src/main/webapp/src/views/questiontype/TrueFalseQuestion.js
@@ -20,6 +20,11 @@ class TrueFalseQuestion extends Component {
         };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+    componentDidMount() {
+        this.setState({
+            question: this.props.question
+        });
+    }
     componentWillReceiveProps(nextProps) {
         if (!this.state.question || (nextProps.question.order !== this.state.question.order)) {
             this.setState({
