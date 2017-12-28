d2e3377a6af96c662039dcf3b24a7435f25618e1
sunminggui <757596945@qq.com>
2017-12-28 14:30:34 +0800

    update

--- web/src/main/webapp/src/views/ScoreShopping.js
+++ web/src/main/webapp/src/views/ScoreShopping.js
@@ -20,19 +20,52 @@ class ScoreShopping extends Component{
     componentDidMount() {
         let _this = this;
         const {host} = _this.props;
-        axios.get(`${host}/score/findTotalScore`).then(function (response) {
+        axios.get(`${host}/score/findAllAwards`).then(function (response) {
             if (response.data.state === 0) {
                 _this.setState({
-                    scoreInfo: response.data.data
+                    awardInfo: response.data.data
                 })
             }
         }).catch(function (errors) {
             console.log(errors);
         })
     }
+    getCurrentAwardScore(type){
+        let awardInfos = this.state.awardInfo;
+        if(type==="TENCENT_VIP"){
+            for(let i=0;i<4;i++){
+                let awardInfo = awardInfos[i];
+                if(awardInfo.awardType=="TENCENT_VIP"){
+                    return awardInfo;
+                }
+            }
+        }else if(type==="OFO_COUPON"){
+            for(let i=0;i<4;i++){
+                let awardInfo = awardInfos[i];
+                if(awardInfo.awardType=="OFO_COUPON"){
+                    return awardInfo;
+                }
+            }
+        }else if(type==="FIVE_COUPON"){
+            for(let i=0;i<4;i++){
+                let awardInfo = awardInfos[i];
+                if(awardInfo.awardType=="FIVE_COUPON"){
+                    return awardInfo;
+                }
+            }
+        }else if(type==="TEN_COUPON"){
+            for(let i=0;i<4;i++){
+                let awardInfo = awardInfos[i];
+                if(awardInfo.awardType=="TEN_COUPON"){
+                    return awardInfo;
+                }
+            }
+        }
+        return null;
+    }
     getCurrentScore(){
-        let scoreInfo = this.state.scoreInfo;
-        let score = scoreInfo?(scoreInfo.score? scoreInfo.score : 0):0;
+        let awardInfos = this.state.awardInfo;
+        let score = awardInfos[0]?0:awardInfos[0].myScore?0:awardInfos[0].myScore;
         return score;
     }
     render(){
@@ -49,38 +82,38 @@ class ScoreShopping extends Component{
                   <div className="largest">
                     <span className="largestImg" />
                       <div className="largestDetail">
-                        <span className="largestSpan">20元充值劵</span>
-                        <span className="scoreSpan">1000积分</span>
+                        <span className="largestSpan">腾讯视频会员月卡</span>
+                        <span className="scoreSpan">{getCurrentAwardScore("TENCENT_VIP").score}积分</span>
                       </div>
-                      <Button disabled={this.getCurrentScore()>1000?false:true}>{this.getCurrentScore()>1000?"立即兑换":"积分不足"}</Button>
+                      <Button disabled={getCurrentAwardScore("TENCENT_VIP").isUsedUp==false?false:true}>{getCurrentAwardScore("TENCENT_VIP").isUsedUp==false?"兑换上限":this.getCurrentScore()>getCurrentAwardScore("TENCENT_VIP").score?"立即兑换":"积分不足"}</Button>
                   </div>
                   <hr className="line"/>
 
                   <div className="larger">
                       <span className="largerImg" />
                       <div className="largerDetail">
-                          <span className="largerSpan">10元充值劵</span>
-                          <span className="scoreSpan">500积分</span>
+                          <span className="largerSpan">ofo共享单车用车券10张1元券</span>
+                          <span className="scoreSpan">{getCurrentAwardScore("OFO_COUPON").score}积分</span>
                       </div>
-                      <Button disabled={this.getCurrentScore()>500?false:true}>{this.getCurrentScore()>500?"立即兑换":"积分不足"}</Button>
+                      <Button disabled={getCurrentAwardScore("OFO_COUPON").isUsedUp==false?false:true}>{getCurrentAwardScore("OFO_COUPON").isUsedUp==false?"兑换上限":this.getCurrentScore()>getCurrentAwardScore("OFO_COUPON").score?"立即兑换":"积分不足"}</Button>
                   </div>
                   <hr className="line"/>
                   <div className="normal">
                       <span className="normalImg" />
                       <div className="normalDetail">
-                          <span className="normalSpan">5元充值劵</span>
-                          <span className="scoreSpan">200积分</span>
+                          <span className="normalSpan">5元优惠券（购物券49元减5元）</span>
+                          <span className="scoreSpan">{getCurrentAwardScore("FIVE_COUPON").score}积分</span>
                       </div>
-                      <Button disabled={this.getCurrentScore()>200?false:true}>{this.getCurrentScore()>200?"立即兑换":"积分不足"}</Button>
+                      <Button disabled={getCurrentAwardScore("FIVE_COUPON").isUsedUp==false?false:true}>{getCurrentAwardScore("FIVE_COUPON").isUsedUp==false?"兑换上限":this.getCurrentScore()>getCurrentAwardScore("FIVE_COUPON").score?"立即兑换":"积分不足"}</Button>
                   </div>
                   <hr className="line"/>
                   <div className="smaller">
                       <span className="smallerImg" />
                       <div className="smallerDetail">
-                          <span className="smallerSpan">2元充值劵</span>
-                          <span className="scoreSpan">100积分</span>
+                          <span className="smallerSpan">10元优惠券（购物券99元减10元）</span>
+                          <span className="scoreSpan">{getCurrentAwardScore("TEN_COUPON").score}积分</span>
                       </div>
-                      <Button disabled={this.getCurrentScore()>100?false:true}>{this.getCurrentScore()>100?"立即兑换":"积分不足"}</Button>
+                      <Button disabled={getCurrentAwardScore("TEN_COUPON").isUsedUp==false?false:true}>{getCurrentAwardScore("TEN_COUPON").isUsedUp==false?"兑换上限":this.getCurrentScore()>getCurrentAwardScore("TEN_COUPON").score?"立即兑换":"积分不足"}</Button>
                   </div>
               </div>
           </div>
