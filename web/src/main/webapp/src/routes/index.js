c8aa4adc4a6671a4c46d087fc4b3b67ad2569c46
sunminggui <757596945@qq.com>
2018-01-03 18:09:57 +0800

    update

--- web/src/main/webapp/src/routes/index.js
+++ web/src/main/webapp/src/routes/index.js
@@ -35,7 +35,7 @@ import MyPrize from '../views/MyPrize';
 /*奖品详情*/
 import PrizeDetail from '../views/PrizeDetail';
 /*兑换成功页面*/
-import ExchangeSuccess from "../views/ExchangeSuccess";
+import ExchangeSuccess from "../views/exchangeSuccess";
 
 const Routes = () => (
     <Switch>
