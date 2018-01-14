fe6d09a9f617d89b468185893bef8cc18683f6cf
sunminggui <757596945@qq.com>
2018-01-14 15:24:45 +0800

    update

--- web/src/main/webapp/src/views/Error.js
+++ web/src/main/webapp/src/views/Error.js
@@ -5,7 +5,7 @@ import qrcode from '../media/images/qrcode.jpg';
 class Error extends Component {
     render() {
         return (
-            <div className='error common-bg'>
+            <div className='error answer-bg'>
                 <img src={qrcode} alt=""/>
                 <p className='text-center'>扫描二维码，关注点击我要答题</p>
             </div>
