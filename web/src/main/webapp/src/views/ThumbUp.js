15a7792c49150d26c55a56f109998393b902913f
sunminggui <757596945@qq.com>
2018-01-14 10:49:13 +0800

    update

--- web/src/main/webapp/src/views/ThumbUp.js
+++ web/src/main/webapp/src/views/ThumbUp.js
@@ -14,7 +14,8 @@ class ThumbUp extends Component {
             scoreInfo: null,
             showToast: false,
             isThumbUp: false,
-            isReady: false
+            isReady: false,
+            alreadyThumbUp:false
         };
         this.handleAnswer = this.handleAnswer.bind(this);
         this.handleThumb = this.handleThumb.bind(this);
@@ -33,7 +34,7 @@ class ThumbUp extends Component {
                 if (response.data.state === 0) {
                     _this.setState({
                         scoreInfo: response.data.data,
-                        isThumbUp: response.data.data.isThumbUp
+                        alreadyThumbUp: response.data.data.isThumbUp
                     })
                 }
             }).catch(function (errors) {
@@ -68,6 +69,7 @@ class ThumbUp extends Component {
     }
     render() {
         let isThumbUp = this.state.isThumbUp;
+        let alreadyThumbUp = this.state.alreadyThumbUp;
         let scoreInfo = this.state.scoreInfo;
         let isReady = this.state.isReady;
         return (
@@ -76,43 +78,62 @@ class ThumbUp extends Component {
                     <img src={header} alt=""/>
                 </div>*/}
                 {
-                    isReady && isThumbUp? (
-                        scoreInfo? (
-                            <div className='wrapper'>
-                                <div className='wrapper-thumb'>
-                                    <div className='third'>
-                                        <p>恭喜你已为好友</p>
-                                        <p>{scoreInfo.NickName}</p>
-                                        <p>点赞成功</p>
-                                    </div>
-                                    <div className='four'>
-                                        <p>我也要争当普法先锋，与好友PK赢奖品</p>
-                                    </div>
+                    isReady && alreadyThumbUp?(
+                        <div className='wrapper'>
+                            <div className='wrapper-thumb'>
+                                <div className='third'>
+                                    <p>你今日已为好友</p>
+                                    <p>{scoreInfo.NickName}</p>
+                                    <p>点过赞了</p>
                                 </div>
-                                <div className='text-center thumb-btn'>
-                                    <div className='right-now' onClick={this.handleAnswer}>马上答题</div>
+                                <div className='four'>
+                                    <p>我也要争当普法先锋，与好友PK赢奖品</p>
                                 </div>
                             </div>
-                        ) : (null)
-                    ) : (
-                        scoreInfo? (
-                            <div className='wrapper'>
-                                <div className='clearfix wrapper-thumb'>
-                                    <div className='pull-left first'>
-                                        <img src={scoreInfo.toPortrait} alt=""/>
-                                        <p>{scoreInfo.toNickName}</p>
-                                    </div>
-                                    <div className='pull-right second'>
-                                        <div>
-                                            <p>为<span>{scoreInfo.toNickName}</span>增加积分领取奖品，力挺TA争当普法先锋</p>
+                            <div className='text-center thumb-btn'>
+                                <div className='right-now' onClick={this.handleAnswer}>马上答题</div>
+                            </div>
+                        </div>
+                    ):(
+                        isReady && isThumbUp? (
+                            scoreInfo? (
+                                <div className='wrapper'>
+                                    <div className='wrapper-thumb'>
+                                        <div className='third'>
+                                            <p>恭喜你已为好友</p>
+                                            <p>{scoreInfo.NickName}</p>
+                                            <p>点赞成功</p>
+                                        </div>
+                                        <div className='four'>
+                                            <P>已有{scoreInfo.thumbUpCount?scoreInfo.thumbUpCount+1:1}人为好友{scoreInfo.NickName}点赞,增加积分{scoreInfo.thumbUpCount?scoreInfo.thumbUpCount+1:1}</P>
+                                            <p>我也要争当普法先锋，与好友PK赢奖品</p>
                                         </div>
                                     </div>
+                                    <div className='text-center thumb-btn'>
+                                        <div className='right-now' onClick={this.handleAnswer}>马上答题</div>
+                                    </div>
                                 </div>
-                                <div className='text-center thumb-btn'>
-                                    <img src={btn_thumbup} onClick={this.handleThumb} alt=""/>
+                            ) : (null)
+                        ) : (
+                            scoreInfo? (
+                                <div className='wrapper'>
+                                    <div className='clearfix wrapper-thumb'>
+                                        <div className='pull-left first'>
+                                            <img src={scoreInfo.toPortrait} alt=""/>
+                                            <p>{scoreInfo.toNickName}</p>
+                                        </div>
+                                        <div className='pull-right second'>
+                                            <div>
+                                                <p>为<span>{scoreInfo.toNickName}</span>增加积分领取奖品，力挺TA争当普法先锋</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='text-center thumb-btn'>
+                                        <img src={btn_thumbup} onClick={this.handleThumb} alt=""/>
+                                    </div>
                                 </div>
-                            </div>
-                        ) : (null)
+                            ) : (null)
+                        )
                     )
                 }
                 <Toast icon="success-no-circle" show={this.state.showToast}>点赞成功</Toast>
