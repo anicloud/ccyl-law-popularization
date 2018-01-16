830fd8efa6b6381920bb1b5be9f7f12e6b51745c
lihui <812486664@qq.com>
2018-01-16 14:26:35 +0800

    update

---
+++ persistence/src/main/java/com/ani/ccyl/leg/persistence/mapper/DailyTotalScoreMapper.java
@@ -0,0 +1,7 @@
+package com.ani.ccyl.leg.persistence.mapper;
+
+import com.ani.ccyl.leg.persistence.mapper.base.SysMapper;
+import com.ani.ccyl.leg.persistence.po.DailyTotalScorePO;
+
+public interface DailyTotalScoreMapper extends SysMapper<DailyTotalScorePO> {
+}
---
+++ persistence/src/main/java/com/ani/ccyl/leg/persistence/service/impl/DailyTotalScorePersistenceServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ani.ccyl.leg.persistence.service.impl;
+
+import com.ani.ccyl.leg.persistence.po.DailyTotalScorePO;
+import com.ani.ccyl.leg.persistence.service.facade.DailyTotalScorePersistenceService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DailyTotalScorePersistenceServiceImpl implements DailyTotalScorePersistenceService {
+    @Override
+    public void updateDailyTotalScore(DailyTotalScorePO dailyTotalScore) {
+
+    }
+}
