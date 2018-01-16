3e123f52a55a6986a430f7cda11cda13755b239f
lihui <812486664@qq.com>
2018-01-16 16:09:12 +0800

    update

--- persistence/src/main/java/com/ani/ccyl/leg/persistence/service/facade/TotalScorePersistenceService.java
+++ persistence/src/main/java/com/ani/ccyl/leg/persistence/service/facade/TotalScorePersistenceService.java
@@ -5,4 +5,8 @@ import com.ani.ccyl.leg.persistence.po.TotalScorePO;
 public interface TotalScorePersistenceService {
     void updateTotalScore(TotalScorePO totalScorePO);
     TotalScorePO findByAccountId(Integer accountId);
+    void updateThumbUp(Integer accountId);
+    void updateSignInCount(Integer accountId);
+    void updateInviteCount(Integer accountId);
+    void updateShareCount(Integer accountId);
 }
